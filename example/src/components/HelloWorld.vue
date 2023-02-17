2d6009e7504bf6f4cf49845a06076d1e9c20a8ca
lqbach <long_quan_bach@hotmail.com>
2023-02-17 04:11:36 -0500

    Updating examples with new v-intro directive

--- example/src/components/HelloWorld.vue
+++ example/src/components/HelloWorld.vue
@@ -1,8 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, inject, onMounted } from 'vue'
+
+const introJs = inject('introJs')
 
 defineProps({
   msg: String,
+})
+
+onMounted(() => {
+  introJs.start()
 })
 
 const count = ref(0)
@@ -11,7 +17,7 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
+  <div class="card" v-intro="'Hi there!'">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
@@ -19,7 +25,7 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
+  <p v-intro="'What is up my friend?'">
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
     official Vue + Vite starter
@@ -29,7 +35,9 @@ const count = ref(0)
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p v-intro="'This is neat!'" class="read-the-docs">
+    Click on the Vite and Vue logos to learn more
+  </p>
 </template>
 
 <style scoped>
--- example/src/main.js
+++ example/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'intro.js/minified/introjs.min.css'
 import App from './App.vue'
+import introJs from 'intro.js-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(introJs)
+
+app.mount('#app')
