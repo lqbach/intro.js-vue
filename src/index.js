0b362c2ed824f9cdc33747a20eb668746a33929b
lqbach <long_quan_bach@hotmail.com>
2023-02-17 04:11:18 -0500

    Changing the build system to accommodate for our library 📚

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import Intro from './intro'
+import Intro from './directives/intro'
+import introJs from 'intro.js'
 
 const plugin = {
-  install(app, options) {
+  install: (app, options) => {
     app.directive('intro', Intro)
+
+    app.provide('introJs', introJs())
   },
-  directive,
 }
 
 export default plugin
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    emptyOutDir: false,
+    lib: {
+      formats: ['es', 'cjs', 'iife'],
+      entry: resolve(__dirname, 'src/index.js'),
+      fileName: 'intro.js-vue',
+      name: 'introJsVue',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        format: 'esm',
+        dir: './dist',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
